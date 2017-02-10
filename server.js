56d699df9e33929771faba582605c480026f9ff0
Roman Zakharenkov <roman.zakharenkov@gmail.com>
2017-02-10 22:53:23 +0200

    Check the repetition

--- server.js
+++ server.js
@@ -17,204 +17,219 @@ var db;
 
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
 
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
 });
 
 // CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
 }
 
 /*
-                Signing In/Loging Out and Signing Up
-  _____________________________________________________________
+ Signing In/Loging Out and Signing Up
+ _____________________________________________________________
 
-  The following route signs the user in /home/giograf by returning sessionToken if hash and
-  username correspond to the entry in the database. Returns false as valid boolean value
-  and "0000" as a security token if the entry is not found. (see README.md)
-*/
-app.post("/users/in", function(req, res) {
-
-});
-
-/*
-  The following route logs the user out by deleting previously granted sessionToken from Sessions.
-  Session token to be supplied with request.
-  collection (see README.md)
-*/
-app.delete("/users/in", function(req, res) {
+ The following route signs the user in /home/giograf by returning sessionToken if hash and
+ username correspond to the entry in the database. Returns false as valid boolean value
+ and "0000" as a security token if the entry is not found. (see README.md)
+ */
+app.post("/users/in", function (req, res) {
 
 });
 
 /*
-  The following route signs up the user by storing user data in
-  Users collection and session in the Sessions collection, returning sessionToken
-  if hash and username correspond to the entry in the database. Returns false as
-  valid boolean value and "0000" as a security token if the entry is not found.
-  (see README.md)
-*/
-app.post("/users", function(req, res) {
-  user_exists = false;
-  db.collection(USERS_COLLECTION).find({username: req.body.username}).toArray(function(err, docs) {
-    if (err) {
-    } else {
-      if(docs.length != 0){
-        user_exists = true;
-      }
-    }
-  });
+ The following route logs the user out by deleting previously granted sessionToken from Sessions.
+ Session token to be supplied with request.
+ collection (see README.md)
+ */
+app.delete("/users/in", function (req, res) {
 
-  if(!user_exists){
-  /* Add new user to DB */
-  db.collection(USERS_COLLECTION).insertOne({ username: req.body.username, hash: req.body.hash, email: req.body.email },
-      function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      newToken = true;
-      do
-      {
-        /* Generate session token */
-        sessionTokenResult = guid();
-        /* Check that the token generated does not yet exist in the DB */
-        db.collection(SESSIONS_COLLECTION).find({sessionToken: sessionTokenResult}).toArray(function(err, docs) {
-          if (err) {
-          } else {
-            if(docs.length != 0){
-              newToken = false;
-            }
-          }
-        });
-      }
-      while(!newToken);
+});
 
-      /*
-        If user has been added to the DB, add the generated session token to the database.
-      */
-      db.collection(SESSIONS_COLLECTION).insertOne({ username: req.body.username, sessionToken: sessionTokenResult},
-          function(err, doc) {
+/*
+ The following route signs up the user by storing user data in
+ Users collection and session in the Sessions collection, returning sessionToken
+ if hash and username correspond to the entry in the database. Returns false as
+ valid boolean value and "0000" as a security token if the entry is not found.
+ (see README.md)
+ */
+app.post("/users", function (req, res) {
+    user_exists = false;
+    checkRepetition = [];
+    // Check that entered username is not in database.
+    db.collection(USERS_COLLECTION).find({username: req.body.username}).toArray(function (err, docs) {
         if (err) {
         } else {
-          returnArray = {"sessionToken": sessionTokenResult, "valid": true};
-          res.status(201).json(returnArray);
+            if (docs.length != 0) {
+                user_exists = true;
+                checkRepetition = docs;
+            }
         }
-      });
+    });
+
+    if (!user_exists) {
+        /* Add new user to DB */
+        db.collection(USERS_COLLECTION).insertOne({
+                username: req.body.username,
+                hash: req.body.hash,
+                email: req.body.email
+            },
+            function (err, doc) {
+                if (err) {
+                    handleError(res, err.message, "Failed to create new contact.");
+                } else {
+                    newToken = true;
+                    do
+                    {
+                        /* Generate session token */
+                        sessionTokenResult = guid();
+                        /* Check that the token generated does not yet exist in the DB */
+                        db.collection(SESSIONS_COLLECTION).find({sessionToken: sessionTokenResult}).toArray(function (err, docs) {
+                            if (err) {
+                            } else {
+                                if (docs.length != 0) {
+                                    newToken = false;
+                                }
+                            }
+                        });
+                    }
+                    while (!newToken);
+
+                    /*
+                     If user has been added to the DB, add the generated session token to the database.
+                     */
+                    db.collection(SESSIONS_COLLECTION).insertOne({
+                            username: req.body.username,
+                            sessionToken: sessionTokenResult
+                        },
+                        function (err, doc) {
+                            if (err) {
+                            } else {
+                                returnArray = {"sessionToken": sessionTokenResult, "valid": true};
+                                res.status(201).json(checkRepetition);
+                            }
+                        });
+                }
+            });
     }
-  });
-  }
+    // If entered username already exists:
+    else {
+        returnArray = {"sessionToken": "0000", "valid": false};
+        res.status(201).json(returnArray);
+    }
 });
 
 
 /*
-                            Search
-  _____________________________________________________________
+ Search
+ _____________________________________________________________
 
-  The following route returns an array of users based on string searched, array
-  containing username, profile picture, userId.
-  (see README.md)
-*/
-app.get("/users/:username", function(req, res) {
-
-});
-
-/*
-  The following route returns an array of images based on string (tag) searched, array
-  containing image URL, imageId and datetime posted
-  (see README.md)
-*/
-app.get("/photos/:tag", function(req, res) {
+ The following route returns an array of users based on string searched, array
+ containing username, profile picture, userId.
+ (see README.md)
+ */
+app.get("/users/:username", function (req, res) {
 
 });
 
 /*
-                            Home
-  _____________________________________________________________
-
-  User provides (implicitly) his username and recieves an array of the most
-  recent photos from users that person follows along with metadata.
-  (see README.md)
-*/
-app.get("/home/:id", function(req, res) {
+ The following route returns an array of images based on string (tag) searched, array
+ containing image URL, imageId and datetime posted
+ (see README.md)
+ */
+app.get("/photos/:tag", function (req, res) {
 
 });
 
 /*
-                            Share
-  _____________________________________________________________
-  An image and metadata is send to server where the photo is saved and in case of
-  success, the result is sent to user as imageId.
-  (see README.md)
-*/
-app.post("/photos", function(req, res) {
+ Home
+ _____________________________________________________________
+
+ User provides (implicitly) his username and recieves an array of the most
+ recent photos from users that person follows along with metadata.
+ (see README.md)
+ */
+app.get("/home/:id", function (req, res) {
 
 });
 
 /*
-                      Photo Interaction
-  _____________________________________________________________
-  Leave a comment under a photo, with imageId, comment and sessionToken supplied
-  (see README.md)
-*/
-app.post("/photos/comment", function(req, res) {
+ Share
+ _____________________________________________________________
+ An image and metadata is send to server where the photo is saved and in case of
+ success, the result is sent to user as imageId.
+ (see README.md)
+ */
+app.post("/photos", function (req, res) {
 
 });
 
 /*
-  Leave a like under photo (unlike if already liked), with imageId
-  and sessionToken supplied
-  (see README.md)
-*/
-app.post("/photos/like", function(req, res) {
+ Photo Interaction
+ _____________________________________________________________
+ Leave a comment under a photo, with imageId, comment and sessionToken supplied
+ (see README.md)
+ */
+app.post("/photos/comment", function (req, res) {
 
 });
 
 /*
-                           Profile
-  _____________________________________________________________
-  Get the profile of the user with the images posted by providing userId
-  (see README.md)
-*/
-app.get("/users/:id", function(req, res) {
+ Leave a like under photo (unlike if already liked), with imageId
+ and sessionToken supplied
+ (see README.md)
+ */
+app.post("/photos/like", function (req, res) {
 
 });
 
 /*
-  Upload new profile picture using sessionToken
-*/
-app.put("/users/profilepicture", function(req, res) {
+ Profile
+ _____________________________________________________________
+ Get the profile of the user with the images posted by providing userId
+ (see README.md)
+ */
+app.get("/users/:id", function (req, res) {
 
 });
 
 /*
-  Follow or unfollow specified user, using sessionToken
-*/
-app.post("/users/follow", function(req, res) {
+ Upload new profile picture using sessionToken
+ */
+app.put("/users/profilepicture", function (req, res) {
 
 });
 
 /*
-                         Photo View
-  _____________________________________________________________
-  Get the photo and all related metadata to the client when imageId is specified.
-  (see README.md)
-*/
-app.get("/photos/:id", function(req, res) {
+ Follow or unfollow specified user, using sessionToken
+ */
+app.post("/users/follow", function (req, res) {
+
+});
+
+/*
+ Photo View
+ _____________________________________________________________
+ Get the photo and all related metadata to the client when imageId is specified.
+ (see README.md)
+ */
+app.get("/photos/:id", function (req, res) {
 
 });
 
@@ -222,12 +237,12 @@ app.get("/photos/:id", function(req, res) {
 
 /* Random Token Generation */
 function guid() {
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-      s4() + '-' + s4() + s4() + s4();
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
 }
 
 function s4() {
-  return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
 }
